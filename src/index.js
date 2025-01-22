564ad0dcbda02b6a607b14bb5968e274e421a926
Jon Atkinson <nosniktaj@icloud.com>
2025-01-22 23:37:30 +0000

    Refactored code and removed redundant styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import Construciton from './construciton.js'
+import Construction from './construction.js'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <Construciton />
+        <Construction/>
     </StrictMode>,
 )
